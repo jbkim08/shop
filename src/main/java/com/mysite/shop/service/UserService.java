f591f49efeea90d18dccfe13c4a51dbe6d493012
JB <drv98@naver.com>
2023-12-28 12:11:45 +0900

    유저 서비스 구현

--- src/main/java/com/mysite/shop/service/UserService.java
+++ src/main/java/com/mysite/shop/service/UserService.java
@@ -1,4 +1,7 @@
 package com.mysite.shop.service;
 
+import com.mysite.shop.model.User;
+
 public interface UserService {
+    User saveUser(User user);
 }
--- src/main/java/com/mysite/shop/service/UserServiceImpl.java
+++ src/main/java/com/mysite/shop/service/UserServiceImpl.java
@@ -1,14 +1,30 @@
 package com.mysite.shop.service;
 
+import com.mysite.shop.model.Role;
+import com.mysite.shop.model.User;
 import com.mysite.shop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
+    //롬북이 생성자를 자동으로 만들어주고 객체는 생성자 주입
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Override
+    public User saveUser(User user){
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        user.setCreateTime(LocalDateTime.now());
+
+        return userRepository.save(user);
+    }
 
 }
