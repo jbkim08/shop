909c3d55a3d055f712921c1a68b3f8fd1bea22ba
JB <drv98@naver.com>
2023-12-28 11:53:23 +0900

    유저 서비스

---
+++ src/main/java/com/mysite/shop/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.mysite.shop.service;
+
+import com.mysite.shop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+
+}
