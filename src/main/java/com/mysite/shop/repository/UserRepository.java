7f27139b784d139fd69e875165a978077b085ddd
JB <drv98@naver.com>
2023-12-28 10:43:24 +0900

    유저리포지토리

--- src/main/java/com/mysite/shop/repository/UserRepository.java
+++ src/main/java/com/mysite/shop/repository/UserRepository.java
@@ -1,4 +1,21 @@
 package com.mysite.shop.repository;
 
-public interface UserRepository {
+import com.mysite.shop.model.Role;
+import com.mysite.shop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //findBy + 열이름(대문자시작)
+    Optional<User> findByUsername(String username);
+
+    //유저네임으로 찾은 유저의 role을 업데이트 함(입력,수정,삭제시 @Modifying 붙음)
+    @Modifying
+    @Query("update User set role=:role where username=:username")
+    void updateUserRole(@Param("username") String username, @Param("role") Role role);
 }
