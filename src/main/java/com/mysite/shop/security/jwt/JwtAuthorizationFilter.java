ef345f83ce555fbc26688f133b907115b189ef65
JB <drv98@naver.com>
2023-12-29 12:47:03 +0900

    가입하기 로그인 테스트

--- src/main/java/com/mysite/shop/security/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/mysite/shop/security/jwt/JwtAuthorizationFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -17,5 +19,12 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        Authentication authentication = jwtProvider.getAuthentication(request);
+        //토큰이 있을경우 바로 인증됨
+        if(authentication != null && jwtProvider.isTokenValid(request)){
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        // 원래 요청으로 이동
+        filterChain.doFilter(request, response);
     }
 }
